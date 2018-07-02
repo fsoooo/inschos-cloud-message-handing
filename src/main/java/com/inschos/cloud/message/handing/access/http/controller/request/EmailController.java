ed2c6b6e5bb520f8370ae2c6fb85b9a83002d64c
meiming <meiming@bigwei.com>
2018-07-02 14:40:14 +0800

    web

--- src/main/java/com/inschos/cloud/message/handing/access/http/controller/request/EmailController.java
+++ src/main/java/com/inschos/cloud/message/handing/access/http/controller/request/EmailController.java
@@ -3,16 +3,13 @@ package com.inschos.cloud.message.handing.access.http.controller.request;
 import com.inschos.cloud.message.handing.access.http.controller.action.EmailAction;
 import com.inschos.cloud.message.handing.access.http.controller.bean.ActionBean;
 import com.inschos.cloud.message.handing.annotation.GetActionBeanAnnotation;
-import com.inschos.cloud.message.handing.assist.kit.HttpKit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
-@RequestMapping("/email")
+@RequestMapping("/web/email")
 public class EmailController {
 
     @Autowired
